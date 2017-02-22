bcdc905e87a7eff78e44b8b87802cc5e6e841945
richardsonjm <richardsonjm@gmail.com>
2017-02-22 11:27:01 -0600

    Pass click function to from product list to product

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,7 @@
 class ProductList extends React.Component {
+  handleProductUpVote(productId) {
+    console.log(productId + ' was upvoted')
+  }
   render() {
     const products = Seed.products.sort((a, b) => (
       b.votes - a.votes
@@ -13,6 +16,7 @@ class ProductList extends React.Component {
         votes={product.votes}
         submitterAvatarUrl={product.submitterAvatarUrl}
         productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
       />
     ));
     return (
@@ -24,6 +28,14 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+  handleUpVote() {
+    this.props.onVote(this.props.id);
+  }
   render() {
     return (
       <div className='item'>
@@ -32,7 +44,7 @@ class Product extends React.Component {
         </div>
         <div className='middle aligned content'>
           <div className='header'>
-            <a>
+            <a onClick={this.handleUpVote}>
               <i className='large caret up icon' />
             </a>
             {this.props.votes}
